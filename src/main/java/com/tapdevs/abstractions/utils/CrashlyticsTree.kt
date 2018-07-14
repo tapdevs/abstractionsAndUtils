025cf05c66464c8a4c77c4d770261057f2fd1481
Jan Shair <janshair@tapdevs.com>
2018-07-14 23:13:43 +0100

    Crashlytics tree

--- src/main/java/com/tapdevs/abstractions/utils/CrashlyticsTree.kt
+++ src/main/java/com/tapdevs/abstractions/utils/CrashlyticsTree.kt
@@ -1,6 +1,7 @@
 package com.tapdevs.abstractions.utils
 
 import android.util.Log
+import com.crashlytics.android.Crashlytics
 import timber.log.Timber
 
 /**
@@ -15,17 +16,17 @@ class CrashlyticsTree : Timber.Tree() {
         }
 
         //TODO: Integrate Crashlytics and uncomment these lines
-        /**
-         * Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
-         * Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
-         * Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
 
-         * if (t == null) {
-         * Crashlytics.logException(new Exception(message));
-         * } else {
-         * Crashlytics.logException(t);
-         * }
-         */
+          Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
+          Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
+          Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
+
+          if (t == null) {
+          Crashlytics.logException(Exception(message));
+          } else {
+          Crashlytics.logException(t);
+          }
+
     }
 
     companion object {
